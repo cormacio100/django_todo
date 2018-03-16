5afdf5df158054c09f689ba36a21d76af07a4753
Cormac Liston <cormac.music@gmail.com>
2018-03-16 21:14:34 +0000

    initial load. Register template loading

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+#   If multiple url.py files exist, app_name allows you specify in TEMPLATES which url file you want to use
+app_name = 'accounts'
+
+urlpatterns = [
+    url(r'^register/',views.register,name='register')
+]
---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+import logging
+logger = logging.getLogger(__name__)
+
+#   Registration View
+def register(request):
+    if request.method =='POST':
+        #   check that the passwords match
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #   Check if user already exists
+                user = User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/register.html',{'error':'Username already in use'})
+            except User.DoesNotExist:
+                logger.debug('logging in user: '+request.POST['username'])
+
+                #   Create User Account
+                user = User.objects.create_user(username=request.POST['username'],email=request.POST['email'],password=request.POST['password'])
+                #   Log the New User in
+                login(request,user)
+
+                logger.debug('the login worked')
+                return render(request,'accounts/register.html')
+        else:
+            args = {'error': 'Passwords did not match'}
+            return render(request,'accounts/register.html',args)
+    else:
+        #   Display an empty form
+        return render(request,'accounts/register.html')
+
